e0c96c0d0a1f7b7cec01c242c0a3c3ae9bc746ae
pandi123 <pandikumar201295@gmail.com>
2016-07-19 09:20:04 +0530

    Create readme.java

---
+++ readme.java
@@ -0,0 +1,15 @@
+# project
+import java.util.Scanner;
+class num
+{
+Scanner s=new Scanner();
+int a=s.nextInt();
+if(a>0)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.println("negative");
+}
+}
